7560b13d41c1dd322e411f7b6ccfb89a351bb098
r3yz0n <reyzon.thapa2020@gmail.com>
2024-08-30 23:53:42 +0545

    production

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   const { isWait, error: postError } = useSelector((state) => state.post);
   const [showLoader, setShowLoader] = useState(true);
   const location = useLocation();
-
+console.log(process.env)
   const fetchData = async () => {
     try {
       await dispatch(fetchAllCategories()).unwrap();
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,14 +1,14 @@
-export const APIURL =
-  process.env.NODE_ENV === "development"
-    ? process.env.REACT_APP_APIURL_DEVELOPMENT
-    : process.env.REACT_APP_APIURL_PRODUCTION;
+// export const APIURL =
+//   process.env.NODE_ENV === "development"
+//     ? process.env.REACT_APP_APIURL_DEVELOPMENT || "http://localhost:5001/api"
+//     : process.env.REACT_APP_APIURL_PRODUCTION ||
+//       "https://default-production-url.com/api";
 
-export const IIMAGE_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:5001/static"
-    : process.env.REACT_APP_IIMAGE_PRODUCTION;
+// export const IIMAGE_URL =
+//   process.env.NODE_ENV === "development"
+//     ? process.env.REACT_APP_IIMAGE_DEVELOPMENT || "http://localhost:5001/static"
+//     : process.env.REACT_APP_IIMAGE_PRODUCTION ||
+//       "https://default-production-url.com/static";
 
-// export const APIURL = ;
-// // export const APIURL = "http://202.79.43.18:5001/api";
-// // export const APIURL = "http://192.168.10.117:5001/api";
-// export const IIMAGE_URL = "http://202.79.43.18:5001/static";
+export const APIURL = process.env.REACT_APP_APIURL;
+export const IIMAGE_URL = process.env.REACT_APP_IIMAGE_URL;
